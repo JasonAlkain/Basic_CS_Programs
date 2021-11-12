0e34db22fa951d24ecd7c418b8e4cb958e9b64a0
Jason <AarsZach@yahoo.com>
2021-11-12 13:49:03 -0700

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,13 @@
+// Welcome to Acme Accounting Systems
+// Remember, we’re “accounting” on you!
+
+using System;
+
+class Program{
+    public static void Main(){
+        string userIn;
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        Console.WriteLine("Remember, we’re “accounting” on you!");
+        Console.ReadLine();
+    }
+}
