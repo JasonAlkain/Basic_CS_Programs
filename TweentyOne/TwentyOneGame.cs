9aa9817580332b43f82b6ad65d2f973c8b1bbfd5
Jason <AarsZach@yahoo.com>
2021-12-05 17:52:47 -0700

    Update TwentyOneGame.cs

--- TweentyOne/TwentyOneGame.cs
+++ TweentyOne/TwentyOneGame.cs
@@ -8,6 +8,15 @@ namespace TweentyOne
 {
     public class TwentyOneGame : Game
     {
+        public override void Play()
+        {
+            
+        }
 
+        public override void ListPlayers()
+        {
+            Console.WriteLine("Welcome Twenty One Players!\n");
+            base.ListPlayers();
+        }
     }
 }
