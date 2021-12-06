5a0c16181b46dbb71e8d2f9c7d0a7adb4a7b768d
Jason <AarsZach@yahoo.com>
2021-12-05 18:26:40 -0700

    Classes update

---
+++ InterfacePolymorphism/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacePolymorphism
+{
+    class Employee : Person, IQuittable
+    {
+        /// <summary>
+        /// "Say my name." - Walter
+        /// </summary>
+        public override void SayName()
+        {
+            Console.WriteLine($"{FirstName} {LastName}");
+        }
+        
+        // Should the employee choose to quit...
+        public void Quit()
+        {
+            Console.WriteLine("You can't quit, your fired.");
+        }
+    }
+}
--- InterfacePolymorphism/Program.cs
+++ InterfacePolymorphism/Program.cs
@@ -8,8 +8,36 @@ namespace InterfacePolymorphism
 {
     class Program
     {
+        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // Simplifiers for Console 'I/O'
+        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        static string ReadLine() => Console.ReadLine();
+        static ConsoleKeyInfo ReadKey() => Console.ReadKey();
+        static void WriteLine(string s) => Console.WriteLine(s);
+        static void Write(string s) => Console.Write(s);
+        static void Clear() => Console.Clear();
+
+        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // Methods
+        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         static void Main(string[] args)
         {
+            // New employee
+            Employee em_1 = new Employee()
+            {
+                FirstName = "Walter",
+                LastName = "[NOT_FOUND]"
+            };
+
+            // Say Hello
+            em_1.SayName();
+
+
+            // Now quit
+            em_1.Quit();
+
+            // Jobs done
+            ReadKey();
         }
     }
 }
