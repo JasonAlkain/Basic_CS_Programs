ac00a6b7005bf1042ac519369bc34897f248f913
Jason <AarsZach@yahoo.com>
2021-12-07 10:54:34 -0700

    Overload the "==" operator
    Checks if two Employee objects are equal by comparing their Id property

---
+++ Operator_Operator_V1/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Operator_V1
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+
+
+
+        public static bool operator ==(Employee emp_1, Employee emp_2)
+            => emp_1.Id == emp_2.Id;
+
+        public static bool operator !=(Employee emp_1, Employee emp_2)
+            => emp_1.Id != emp_2.Id;
+    }
+}
--- Operator_Operator_V1/Program.cs
+++ Operator_Operator_V1/Program.cs
@@ -22,6 +22,38 @@ namespace Operator_Operator_V1
         // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         static void Main(string[] args)
         {
+            // Employee objects to compare
+            Employee emp_A = new Employee() { Id = 1 };
+            Employee emp_B = new Employee() { Id = 2 };
+            Employee emp_C = new Employee() { Id = 2 };
+            
+            // Temp string variable
+            string _ = string.Empty;
+
+            
+            
+            // compare Employee A to B
+            _ = emp_A == emp_B ? "yes" : "No";
+
+            // Print question and result
+            WriteLine($"Does the Id of employee A equal employee B?");
+            WriteLine($"{_}");
+
+            // compare Employee A to C
+            _ = emp_A == emp_C ? "yes" : "No";
+
+            // Print question and result
+            WriteLine($"Does the Id of employee A equal employee C?");
+            WriteLine($"{_}");
+
+            // compare Employee C to B
+            _ = emp_C == emp_B ? "yes" : "No";
+
+            // Print question and result
+            WriteLine($"Does the Id of employee C equal employee B?");
+            WriteLine($"{_}");
+
+            ReadKey();
         }
     }
 }
