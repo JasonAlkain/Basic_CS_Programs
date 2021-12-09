aa172f2d007b5a902b811b1b55e54d7968b21662
Jason <AarsZach@yahoo.com>
2021-12-09 13:39:24 -0700

    Date and Time Update
    A programmer happened here.

--- Date_And_Time/Program.cs
+++ Date_And_Time/Program.cs
@@ -22,6 +22,29 @@ namespace Date_And_Time
         // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         static void Main(string[] args)
         {
+            // This is the date and time... well... now...
+            // "It's simple spell but quite unbreakable" - Dr. Strange
+            WriteLine($"{DateTime.Now}");
+            // Meme? I don't know I don't really get it. But maybe it's funny?
+            WriteLine("I am once again asking you for a number.");
+            // Flare to make it look nice
+            Write(">>: ");
+            // either you get a number or you get 0
+            int.TryParse(ReadLine(), out int userIn);
+
+            // Clears the screen
+            // BUT only if you uncomment it...
+            // Clear();
+
+            // Respond
+            WriteLine("\nSilly me I forgot to tell you why here is the hours we skipped ahead.");
+
+            // Print DateTime.Now with the added hours from the user.
+            WriteLine($"{DateTime.Now.AddHours(userIn)}");
+
+
+            // End of the road. (or program in this case)
+            ReadKey();
         }
     }
 }
