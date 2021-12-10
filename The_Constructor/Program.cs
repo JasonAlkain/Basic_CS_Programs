2b92f96131b13665831408e97a96869b3ef4e946
Jason <AarsZach@yahoo.com>
2021-12-09 19:43:04 -0700

    The Constructor Update
    I did a thing....

--- The_Constructor/Program.cs
+++ The_Constructor/Program.cs
@@ -22,6 +22,42 @@ namespace The_Constructor
         // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         static void Main(string[] args)
         {
+            const int MOD_ALL = 9;
+            var rnd = new Random();
+            int rnd_A = rnd.Next() % MOD_ALL;
+            int rnd_B = rnd.Next() % MOD_ALL;
+
+            // Based constructor
+            var cc_A = new ChainedClass(rnd_A, rnd_B);
+            // Chained constructor
+            var cc_B = new ChainedClass(rnd_A + rnd_B);
+
+            // Print results
+            WriteLine("Not Chained:");
+            WriteLine($"{cc_A.Alpha} | {cc_A.Beta}");
+            WriteLine("Chained:");
+            WriteLine($"{cc_B.Alpha} | {cc_B.Beta}");
+
+            ReadKey();
+        }
+    }
+
+    public class ChainedClass
+    {
+        public int Alpha { get; set; }
+        public int Beta { get; set; }
+
+
+
+        public ChainedClass(int alpha) : this(alpha, new Random().Next() % 9)
+        {
+
+        }
+
+        public ChainedClass(int alpha, int beta)
+        {
+            Alpha = alpha;
+            Beta = beta;
         }
     }
 }
