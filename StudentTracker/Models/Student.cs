5c475830be3f45fd2895396abace7bfbd3030ce6
Jason <AarsZach@yahoo.com>
2021-12-13 03:54:20 -0700

    Student Tracker

---
+++ StudentTracker/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentTracker.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string Student_FName { get; set; }
+        public string Student_LName { get; set; }
+        public string StudentEmail { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+
+        //fully defined relationship
+        public int? GradeId { get; set; }
+        public virtual string Course { get; set; }
+    }
+}
