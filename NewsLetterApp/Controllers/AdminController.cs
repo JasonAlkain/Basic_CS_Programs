88c75f93948d7b516b9554375694890191c93f45
Jason <AarsZach@yahoo.com>
2021-12-12 21:03:12 -0700

    Video Follow Along

---
+++ NewsLetterApp/Controllers/AdminController.cs
@@ -0,0 +1,71 @@
+﻿using NewsLetterApp.Models;
+using NewsLetterApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewsLetterApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Newletter;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+
+
+
+            string queryString = @"SELECT Id, FirstName, LastName, EmailAddress FROM SignUps";
+            List<NewsLetterSignUp> signUps = new List<NewsLetterSignUp>();
+
+            //using (SqlConnection connection = new SqlConnection(connectionString))
+            //{
+            //    SqlCommand command = new SqlCommand(queryString, connection);
+
+            //    connection.Open();
+
+            //    SqlDataReader reader = command.ExecuteReader();
+
+            //    while (reader.Read())
+            //    {
+            //        var signup = new NewsLetterSignUp();
+            //        signup.Id = Convert.ToInt32(reader["Id"]);
+            //        signup.FirstName = reader["FirstName"].ToString();
+            //        signup.LastName = reader["LastName"].ToString();
+            //        signup.EmailAddress = reader["EmailAddress"].ToString();
+
+            //        signUps.Add(signup);
+            //    }
+            //}
+            var signUpVMs = new List<SignUpVM>();
+
+            foreach (var signUp in signUps)
+            {
+                var su_Vm = new SignUpVM();
+                su_Vm.Id = signUp.Id;
+                su_Vm.FirstName = signUp.FirstName;
+                su_Vm.LastName = signUp.LastName;
+                su_Vm.EmailAddress = signUp.EmailAddress;
+
+                signUpVMs.Add(su_Vm);
+            }
+
+            //
+            return View(signUpVMs);
+        }
+
+        public ActionResult Unsubscribe(int Id)
+        {
+            //using(NewsLetterEntities db = new NewsLetterEntities())
+            //{
+
+            //}
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ NewsLetterApp/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using NewsLetterApp.Models;
+using NewsLetterApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewsLetterApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Newletter;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(string firstName, string lastName, string emailAddress)
+        {
+            bool isNotValid = string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress);
+            if (isNotValid)
+            {
+                return View("~/Views/Shared/Error.cshtml");
+            }
+            else
+            {
+
+                /* TODO:
+                 * Connect to server database 
+                 */
+                string queryString = @"INSERT INTO SignUps (FirstName, LastName, EmailAddress) VALUES
+                                        (@FirstName, @LastName, @EmailAddress)";
+
+                using(SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlCommand command = new SqlCommand(queryString, connection);
+                    command.Parameters.Add("@FirstName", System.Data.SqlDbType.VarChar);
+                    command.Parameters.Add("@LastName", System.Data.SqlDbType.VarChar);
+                    command.Parameters.Add("@EmailAddress", System.Data.SqlDbType.VarChar);
+
+                    command.Parameters["@FirstName"].Value = firstName;
+                    command.Parameters["@LastName"].Value = lastName;
+                    command.Parameters["@EmailAddress"].Value = emailAddress;
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+                return View("Success");
+            }
+        }
+
+    }
+}
